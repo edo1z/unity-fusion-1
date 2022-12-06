977d2e1ce9ef1c7b67b9ed747a1ae7a165587a8f
web3ten0 <web3ten0@gmail.com>
2022-12-06 23:36:49 +0900

    PlayerがBallを飛ばせるようになった

--- Assets/NetworkInputData.cs
+++ Assets/NetworkInputData.cs
@@ -5,5 +5,8 @@ using UnityEngine;
 
 public struct NetworkInputData : INetworkInput
 {
+  public const byte MOUSEBUTTON1 = 0x01;
+
+  public byte buttons;
   public Vector3 direction;
 }
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,11 +5,15 @@ using Fusion;
 
 public class Player : NetworkBehaviour
 {
+  [SerializeField] private Ball _prefabBall;
+  [Networked] private TickTimer delay { get; set; }
+  private Vector3 _forward;
   private NetworkCharacterControllerPrototype _cc;
 
   private void Awake()
   {
     _cc = GetComponent<NetworkCharacterControllerPrototype>();
+    _forward = transform.forward;
   }
 
   public override void FixedUpdateNetwork()
@@ -18,6 +22,29 @@ public class Player : NetworkBehaviour
     {
       data.direction.Normalize();
       _cc.Move(5 * data.direction * Runner.DeltaTime);
+
+      // Ball
+      if (data.direction.sqrMagnitude > 0)
+      {
+        _forward = data.direction;
+      }
+      if (delay.ExpiredOrNotRunning(Runner))
+      {
+        if ((data.buttons & NetworkInputData.MOUSEBUTTON1) != 0)
+        {
+          delay = TickTimer.CreateFromSeconds(Runner, 0.5f);
+          Runner.Spawn(
+            _prefabBall,
+            transform.position + _forward,
+            Quaternion.LookRotation(_forward),
+            Object.InputAuthority,
+            (runner, o) =>
+            {
+              o.GetComponent<Ball>().Init();
+            }
+          );
+        }
+      }
     }
   }
 }
